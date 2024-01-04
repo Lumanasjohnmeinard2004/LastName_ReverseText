e652ca1abfb5bcb6d637db778ab3988e32a785e5
Lumanasjohnmeinard2004 <145661489+Lumanasjohnmeinard2004@users.noreply.github.com>
2024-01-04 22:42:36 +0800

    Add files via upload

---
+++ Lumanas_ReverseText_Function.py
@@ -0,0 +1,13 @@
+def reverse_string():
+  while True:
+    try:
+      text = input("Enter a string: ")
+      reversed_text = ""
+      for char in text:
+        reversed_text = char + reversed_text
+      print("Output:", reversed_text)
+      break
+    except ValueError:
+      print("Error Reported! Enter text only.")
+
+reverse_string()
